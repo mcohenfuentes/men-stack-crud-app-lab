49b24738e2d77d3737b264e84a6743bc77522523
Mica CF <mcohenfuentes@gmail.com>
2025-02-14 19:57:01 -0500

    creating index page

--- server.js
+++ server.js
@@ -13,14 +13,35 @@ mongoose.connection.on("connected", () => {
   });
 
   const Cheese = require("./models/cheese.js");
+  
+  //middleware
+  app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
     res.render('index.ejs');
   });
 
+  app.get("/cheeses", async (req, res) => {
+    const allCheeses = await Cheese.find()
+    console.log(allCheeses)
+    res.render("cheeses/index.ejs", { cheeses: allCheeses });
+  });
+
   app.get("/cheeses/new", (req, res) => {
     res.render('cheeses/new.ejs');
   });
+
+  app.post("/cheeses", async (req, res) => {
+    if (req.body.isStinky === "on") {
+      req.body.isStinky = true;
+    } else {
+      req.body.isStinky = false;
+    }
+    await Cheese.create(req.body);
+    res.redirect("/cheeses/new");
+    console.log(req.body);
+  });
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
